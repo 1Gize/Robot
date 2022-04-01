296023e41b2f4f5a7203f3fd16c3a0eaee06456c
1Gize <grzolko@gmail.com>
2022-04-01 16:27:32 +0200

    finnished the task

--- src/test/scala/RobotSpec.scala
+++ src/test/scala/RobotSpec.scala
@@ -2,7 +2,7 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
 class RobotSpec extends AnyFlatSpec with should.Matchers{
-  it should "return robot postition(8,5) facing North for input  RAALAL and starting Robot(7,3,north)" in {
+  it should "return robot postition(8,5) facing west for input  RAALAL and starting Robot(7,3,north)" in {
     val rob = new Robot(7,3,"north")
     rob.execCommadns("RAALAL")
     rob.x shouldBe 8
